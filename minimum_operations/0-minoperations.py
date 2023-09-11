90b2cecbaa9bc754194b338463367be028d1e37a
Pierrette Umutoniwase <116735775+Umutoniwasepie@users.noreply.github.com>
2023-09-11 12:05:58 +0200

    minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -2,25 +2,21 @@
 """
 Calculates the fewest number of operations needed to result in exactly n H characters in the file.
 """
+import math
+
 
 def minOperations(n):
+    """Minimum Operations"""
     if n <= 1:
         return 0
 
-    operations = [float('inf')] * (n + 1)
-    operations[1] = 0
+    operations = 0
+    for i in range(2, int(math.sqrt(abs(n))) + 1):
+        while n % i == 0:
+            operations += i
+            n //= i
 
-    for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:
-                operations[i] = min(operations[i], operations[j] + (i // j))
+    if n > 1:
+        operations += n
 
-    return operations[n] if operations[n] != float('inf') else 0
-
-if __name__ == "__main__":
-    n = 4
-    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
-
-    n = 12
-    print("Min number of operations to reach {} characters: {}".format(n, minOperations(n)))
-
+    return operations
