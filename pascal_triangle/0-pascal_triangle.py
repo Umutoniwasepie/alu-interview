b23260e440f48ea9b0069961b59f274fa4275877
Umutoniwasepie <p.umutoniwa@alustudent.com>
2023-10-19 01:52:35 -0700

    pascal's triangle

---
+++ pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+
+    triangle = []
+    for i in range(n):
+        if i == 0:
+            triangle.append([1])
+        else:
+            row = [1]
+            for j in range(1, i):
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            row.append(1)
+            triangle.append(row)
+
+    return triangle
+
+if __name__ == "__main__":
+    triangle = pascal_triangle(5)
+    for row in triangle:
+        print(row)
+
